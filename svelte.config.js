5b7b1ab9dc6f8ba9c5bbf1816b7bfd277b65810b
Maan Khalife <maan.khalife@yahoo.com>
2022-06-02 21:25:11 +0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-auto';
 import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +9,20 @@ const config = {
 		}),
 	  ],
 	kit: {
-		adapter: adapter(),
+		adapter: vercel({
+			// if true, will deploy the app using edge functions
+			// (https://vercel.com/docs/concepts/functions/edge-functions)
+			// rather than serverless functions
+			edge: false,
+	  
+			// an array of dependencies that esbuild should treat
+			// as external when bundling functions
+			external: [],
+	  
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
+		  }),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
@@ -19,23 +31,4 @@ const config = {
 	}
 };
 
-export default {
-	kit: {
-	  // default options are shown
-	  adapter: vercel({
-		// if true, will deploy the app using edge functions
-		// (https://vercel.com/docs/concepts/functions/edge-functions)
-		// rather than serverless functions
-		edge: false,
-  
-		// an array of dependencies that esbuild should treat
-		// as external when bundling functions
-		external: [],
-  
-		// if true, will split your app into multiple functions
-		// instead of creating a single one for the entire app
-		split: false
-	  })
-	}
-  };
-//export default config;
+export default config;
